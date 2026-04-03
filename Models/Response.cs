295fc2c938b28cdf793dc2d398e40aa87f7c19ff
moraisgomes123 <moraisgomesadaochabango@gmail.com>
2026-04-03 17:19:36 +0200

    Initialize properties with default values in Response class

--- Models/Response.cs
+++ Models/Response.cs
@@ -1,8 +1,16 @@
-﻿namespace CybersecurityChatbot.Models
+namespace CybersecurityChatbot.Models
 {
     public class Response
     {
-        public string Keyword { get; set; }
-        public string Reply { get; set; }
+        public string Keyword { get; set; } = "";
+        public string Reply { get; set; } = "";
+
+        public Response() { }
+
+        public Response(string keyword, string reply)
+        {
+            Keyword = keyword ?? "";
+            Reply = reply ?? "";
+        }
     }
 }
